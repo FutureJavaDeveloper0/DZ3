3a075ebc5cfdd8d459a9665ab517a06a5ddaa6b7
Denial <den4ic369@gmail.com>
2023-03-25 16:47:29 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref} from "vue";
+import style from "./cardStyle.module.css";
+
+const cars = ref([
+  {car: "Toyota"},
+  {car: "BMW"},
+  {car: "Mercedes"},
+  {car: "Lamborghini"},
+  {car: "Tokada"},
+]);
+// const cars = ref(["car1","car2","car3"]);
+const text = ref("");
+
+</script>
+
+<template>
+  <input type="text" v-model="text"/>
+  <template  v-for="{car} in cars" :key="car.value">
+    <div  :class = "style.car" v-if="car.toLowerCase().includes(text.toLocaleLowerCase())">
+      <h1>{{ car }}</h1>
+    </div>
+  </template>
+</template>
